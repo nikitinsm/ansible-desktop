82121982dc4323d641a802b477ad976d1c53d756
Sergey Nikitin <nikitinsm@gmail.com>
2015-10-11 08:48:02 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,12 +15,13 @@ sudo apt-get install -y git python-pip python-dev openssh-server libyaml-cpp-dev
 sudo pip install -U pip ansible
 
 # Configure ansible inventory
-sudo mkdir /etc/ansible
+sudo mkdir /etc/ansible || true
 sudo chmod 777 /etc/ansible
 sudo echo "localhost ansible_connection=local" > /etc/ansible/hosts
 
 # Prepare repository
-sudo mkdir -p /opt/nikitinsm/ && cd /opt/nikitinsm/
+sudo mkdir -p /opt/nikitinsm/ || true
+cd /opt/nikitinsm/
 sudo chown -R $current_user:$current_user ./
 git clone https://github.com/nikitinsm/ansible-desktop.git && cd ansible-desktop
 
